71e6321dd6e0dc13959bb85e97ba3e438c08e54d
cuellarcarla <146938245+cuellarcarla@users.noreply.github.com>
2026-03-31 12:58:54 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "mi-terraform-state-12345"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"  
 }
